813889e69b0785a1433afa9d252383cedbea36fa
Qifan Guo <qifanguo@gmail.com>
2019-07-20 14:08:39 -0400

    connected to mongodb database

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 var express = require('express');
-var mongoose = require ('mongoose');
-var bodyparser = require("body-parser");
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
 var path = require('path');
+var cors = require('cors');
 
 var app = express();
+var route = require('./routes/route');
 
+//middleware
+app.use(cors());
+app.use(bodyparser.json());
+
+//connect to mongoDB
+mongoose.connect('mongodb://localhost:27017/whatever');
+
+
+mongoose.connection.on("err",(err)=>{
+    if(err){
+        console.log('hey shit does not work');
+    }else {
+        console.log('hey shit works');
+    }
+    
+});
+
+mongoose.connection.on("connected",()=>{
+    console.log("connected to db");
+    
+});
 //port number
 var port = 3000;
+
+//route
+app.use('/api',route);
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
     res.send("qifan is here !");
--- practice/index.js
+++ practice/index.js
@@ -1,47 +1,50 @@
 var http = require('http');
 var fs = require('fs');
-function handle_request_qifan(req,res){
-    var body ="";
+
+function handle_request_qifan(req, res) {
+    var body = "";
     console.log("incoming traffic");
-    res.writeHead(200,{"Content-Type" : "application/json"});
-    read_text_file((err,text)=>{
-        if(err){
-            console.log("shit going down in here"+err);
-        }
-        else{
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    read_text_file((err, text) => {
+        if (err) {
+            console.log("shit going down in here" + err);
+        } else {
             body = text;
-            res.end(body+"\n");
+            res.end(body + "\n");
         }
     });
 
-    
+
 }
-function read_text_file(callback){
-    find_file((err,filenames)=>{
-        if(err){
+
+function read_text_file(callback) {
+    find_file((err, filenames) => {
+        if (err) {
             console.log(err);
-        }else{
-            fs.readFile(filenames[1],'utf8',(err,text)=>{
-                if(err){
+        } else {
+            fs.readFile(filenames[1], 'utf8', (err, text) => {
+                if (err) {
                     callback(err);
-            }else{
-                    callback(null,text);
-                
-            }
-                    
-               
+                } else {
+                    callback(null, text);
+
+                }
+
+
             })
         }
     })
 }
 
-function find_file(callback){
-    fs.readdir('./',(err,filenames)=>{
-        if(err){
+function find_file(callback) {
+    fs.readdir('./', (err, filenames) => {
+        if (err) {
             callback(err);
             return;
-        }else{
-            callback(null,filenames);
+        } else {
+            callback(null, filenames);
         }
     });
 }
@@ -49,4 +52,3 @@ function find_file(callback){
 var server = http.createServer(handle_request_qifan);
 server.listen(8080);
 console.log("Server has started, listening on port 8080 local");
-
