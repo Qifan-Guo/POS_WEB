10a8ee50404648c673ab1821aad655cd3c868ee2
qifan guo <qifanguo@gmail.com>
2019-07-20 21:24:45 -0400

    backend basic funtion complete

--- routes/route.js
+++ routes/route.js
@@ -1,15 +1,46 @@
 const express = require('express');
 const router = express.Router();
+var ObjectId = require('mongodb').ObjectID;
+const order = require('../models/order');
+
 
 router.get('/order',(req,res,next)=>{
-    res.send("start ordering");
+    order.find((err,orders)=>{
+        if(err){
+            res.json(err);
+        }else{
+            res.json(orders);
+        }
+    })
 });
 
+//order properties:
+//item_name:string
+//price:number
 router.post('/order',(req,res,next)=>{
-    //add order logic goes here
+    let newOrder = new order({
+        item_name: req.body.item_name,
+        price: req.body.price
+    });
+    newOrder.save((err,order)=>{
+        if(err){
+            res.json({msg: 'failed to add order'});
+        }else{
+            res.json({msg: 'order added successfully'});
+        }
+
+    })
 });
 
-router.delete('/order',(req,res,next)=>{
-    //delete order logic goes here 
+router.delete('/order/:id',(req,res,next)=>{
+    
+    order.deleteOne({"_id":req.params.id},(err,result)=>{
+        if(err){
+            res.json({msg: 'Error:cannot remove order'});
+        }else{
+            res.json(result);
+            
+             }
+    })
 });
 module.exports = router;
